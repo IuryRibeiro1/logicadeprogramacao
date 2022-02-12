db496b186cd44d42500a242454a727761ba8839f
Iurysyu <iury211@gmail.com>
2022-02-12 13:12:15 -0300

    Atividade 2 - Lógica de Programação

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log ("Insira a idade: ");
+Number = 18; 
+
+if (18 >= 18) console.log ("Cadastro Permitido")
+else console.log ("Cadastro não permitido");
+
+var listaDeCadastro = 0
+
+while (listaDeCadastro <= 100) { console.log("Cadastro permitido");
+listaDeCadastro++
+
+}
+if (listaDeCadastro >= 100) console.log ("Cadastro não permitido por excesso de vagas");
+
